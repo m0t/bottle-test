1a462c15c9f0f5f8b7bfcde1ffb2314600d8a3a4
m0t <m0t@users.noreply.github.com>
2014-07-05 22:06:46 +0100

    bottle

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import sys
 #@route('/hello/<name>')
 @route('/')
 def index():
-    return template('<h1>Helo world!</h1>')
+    return '<h1>Helo world!</h1>'
     #return template('<b>Hello {{name}}</b>!', name=name)
 
 run(host='localhost', port=sys.argv[1])
